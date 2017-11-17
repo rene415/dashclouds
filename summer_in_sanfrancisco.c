b0a65fabafe1973844b793a01eccdbb4b7908b2e
Rene Ramirez <rramirez@e1z3r7p17.42.us.org>
2017-11-16 16:23:05 -0800

    i give up

---
+++ summer_in_sanfrancisco.c
@@ -0,0 +1,90 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char const *str)
+{
+	if(!str)
+		return ;
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+void	summer_in_sanfrancisco(int clouds)
+{
+	int i;
+	int x;
+
+	x = 0;
+	i = 0;
+	if (clouds < 1)
+		return ;
+	if (clouds == 1)
+	{
+		ft_putstr(" ~^~^~^~\n~^{CLOUD}^~\n~^{CLOUD}^~\n ~^~^~^~\n / / / /\n/ / / /\n / / /\n");
+	}
+	if(x < clouds)
+	{
+		while(i < clouds)
+		{
+			ft_putstr("  ~^~^~^~  ");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while(i < clouds)
+		{
+			ft_putstr("~^{CLOUD}^~");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while(i < clouds)
+		{
+			ft_putstr("~^{CLOUD}^~");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while(i < clouds)
+		{
+			ft_putstr("  ~^~^~^~  ");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while(i < clouds)
+		{
+			ft_putstr(" / / / /");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while (i < clouds)
+		{
+			ft_putstr("/ / / /");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		i = 0;
+		while(i < clouds)
+		{
+			ft_putstr(" / / /");
+				i++;
+		}
+		if (i == clouds)
+			ft_putchar('\n');
+		x++;
+	}
+}
